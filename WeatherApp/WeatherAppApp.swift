cd706dd97d90069e77fd77855a6d0cef47e43e8e
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2026-03-12 12:29:17 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Daniel Grosjean on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
